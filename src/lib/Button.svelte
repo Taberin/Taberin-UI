84b8537254dc0e75322b17941a6bcfcf7c924f2f
Yoza Wiratama <y_wiratama@yahoo.com>
2025-01-20 22:00:20 +0700

    button component ready to use

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,7 +1,52 @@
 <script lang="ts">
-    let { type, disabled }: { type: "button" | "submit" | "reset" | null | undefined; disabled: boolean } = $props();
+    import type { MouseEventHandler } from "svelte/elements";
+    import type { colorType } from "../types/types.js";
+
+    export let type: "button" | "submit" | "reset" = "button";
+    export let disabled: boolean = false;
+    export let color: colorType = null;
+    export let basic: boolean = false;
+    export let inverted: boolean = false;
+    export let state: "active" | "disabled" | "loading" | null | undefined =
+        null;
+    export let size:
+        | "mini"
+        | "tiny"
+        | "small"
+        | "medium"
+        | "large"
+        | "big"
+        | "huge"
+        | "massive" = "medium";
+    export let float: "right" | "left" | null | undefined = null;
+    export let compact: boolean = false;
+    export let fluid: boolean = false;
+
+    export let onclick: MouseEventHandler<HTMLButtonElement> | null = null;
+    export let ondblclick: MouseEventHandler<HTMLButtonElement> | null = null;
+
+    let classNames = [
+        "ui",
+        color,
+        basic ? "basic" : "",
+        inverted ? "inverted" : "",
+        state,
+        float ? `${float} floated` : "",
+        compact ? "compact" : "",
+        fluid ? "fluid" : "",
+        size,
+        "button",
+    ]
+        .filter(Boolean)
+        .join(" ");
 </script>
 
-<button class="ui button" {type} {disabled}>
+<button
+    class={classNames}
+    type={type || undefined}
+    {disabled}
+    {onclick}
+    {ondblclick}
+>
     <slot></slot>
 </button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
-    import '../styles/semantic.css'
+    import "../styles/semantic.css";
     import Button from "$lib/Button.svelte";
+
+    function handleClick(event: { detail: any }) {
+        console.log("Click event:", event.detail); // { state, timestamp }
+    }
+
 </script>
 
-
-<Button type="button" disabled={false}>Press Me!</Button>
+<Button
+    type="button"
+    color="black"
+    onclick={handleClick}>Press Me!</Button
+>
